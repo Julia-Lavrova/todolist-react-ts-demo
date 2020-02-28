cc2e4cd7166d3c3685c9bbbd36eaf289bed858b8
Julia Lavrova <j.v.lavrova@gmail.com>
2020-02-28 14:46:49 +0300

    Updates

---
+++ precache-manifest.f4c4137364498e83fd7d8b79cbc7408d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d322bec650f1c71ba17ad40109e7491",
+    "url": "/todolist-react-ts-demo/index.html"
+  },
+  {
+    "revision": "4b82045997bf98a1b2e8",
+    "url": "/todolist-react-ts-demo/static/css/main.7a4421ba.chunk.css"
+  },
+  {
+    "revision": "0fbb14f12b47e2511d1f",
+    "url": "/todolist-react-ts-demo/static/js/2.69bef2da.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/todolist-react-ts-demo/static/js/2.69bef2da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b82045997bf98a1b2e8",
+    "url": "/todolist-react-ts-demo/static/js/main.c6b0595a.chunk.js"
+  },
+  {
+    "revision": "20baa11955cf53977322",
+    "url": "/todolist-react-ts-demo/static/js/runtime-main.ce9b12d8.js"
+  },
+  {
+    "revision": "ae97d51f33e595061dee37cd44e44e98",
+    "url": "/todolist-react-ts-demo/static/media/remove.ae97d51f.svg"
+  }
+]);
